47d6cfe35104417da3b2ecd51b387d38488c786d
Kahve Digital <selim@kahvedigital.com>
2021-02-21 17:48:01 +0300

    Update moka_payment.php

--- catalog/controller/payment/moka_payment.php
+++ catalog/controller/payment/moka_payment.php
@@ -52,7 +52,7 @@ class ControllerPaymentMokaPayment extends Controller {
 
             $hashValue = $_POST['hashValue'];
             $HashSession = SHA256($this->session->data['CodeForHash']+"T");
-            if ($hashValue = $HashSession) {
+            if ($hashValue == $HashSession) {
                 $record['result'] = true;
             } else {
                 $record['result'] = false;
